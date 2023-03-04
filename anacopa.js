15b353a480b1608b281f8a3ce361ea2afe0682ed
kkovalsky <biologakovalsky@gmail.com>
2023-03-04 16:25:07 -0300

    insere arquivos do exercicio

---
+++ anacopa.js
@@ -0,0 +1,11 @@
+//o ano da primeira copa foi 1930
+//o ano da próxima copa será em 2016
+
+let anoCopa = 1930;
+//Esta variável inicializa com 
+let ultimaCopa = 2026;
+
+while(anaCopa <+ ultimaCopaAno+){
+    console.log('teve copa em: ${anaCopa}')
+    anacopa = anocopa + 4
+}
---
+++ for.js
@@ -0,0 +1,13 @@
+//estrutura de repetição//
+
+for (let i = 0; i <= 10; i++) {
+console.log(i
+
+    //tabuada//
+
+    const tabuada = 4
+
+    for (let i = 1; i <= 10, i++){
+console.log('${tabuada} + "x" + i +")
+
+    }
